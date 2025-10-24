c64a4d48f076c8aea8e50d31d5ad190cd4031543
Sunwhver <dianaalveszamprogna@gmail.com>
2025-10-23 21:59:28 -0300

    aula com tailwindcss e importacao

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from "./components/layout/Header"
+import { Footer } from "./components/layout/footer"
+
 
 function App() {
-  
+
   return (
     <>
-      <Header />
+    <Header/>
+    <main></main>
+    <Footer />
+
+    
     </>
   )
 }
